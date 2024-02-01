9ed6337e36c44e2ec36734f6e5d4b8b691785334
Vitali Tseluiev <ipz221_tsvo@student.ztu.edu.ua>
2024-02-02 00:03:36 +0200

    Console Project C#

---
+++ ConsoleProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine("Введіть перше число:");
+        if (double.TryParse(Console.ReadLine(), out double number1))
+        {
+            Console.WriteLine("Введіть друге число:");
+            if (double.TryParse(Console.ReadLine(), out double number2))
+            {
+                double sum = number1 + number2;
+                Console.WriteLine($"Сума: {sum}");
+            }
+        }
+    }
+}
